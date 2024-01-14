cff8812e25ef8abb5f1d11e90327b0eedb9fc055
tabrez510 <alamtabrez510@gmail.com>
2024-01-14 14:16:31 +0530

    done with reading and writing file

---
+++ app.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body>')
+        
+        let message ;
+        try{
+            message = fs.readFileSync('message.txt', 'utf8');
+        }
+        catch(error){
+            message=undefined;
+        }
+        res.write('<p>Previous Message: ' + (message !== undefined ? message : 'undefined') + '</p>');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method==='POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        })
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.end();
+})
+
+server.listen(4000);
