78399c9d5d9925d36716eb8121dfd97907bb5a83
tabrez510 <alamtabrez510@gmail.com>
2024-01-14 18:33:33 +0530

    added node modules system

---
+++ routes.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body>')
+        
+        let message ;
+        try{
+            message = fs.readFileSync('message.txt', 'utf8');
+        }
+        catch(error){
+            message=undefined;
+        }
+        res.write('<p>Previous Message: ' + (message !== undefined ? message : 'undefined') + '</p>');
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</body>')
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/message' && method==='POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, () => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/')
+                return res.end();
+            });
+        })
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title><head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+}
+
+// module.exports = requestHandler;
+
+module.exports = {
+    handler : requestHandler
+    // someText : 'Hello'
+}
+
+// exports.handler = requestHandler;
+// exports.someText = 'Hello';
+
